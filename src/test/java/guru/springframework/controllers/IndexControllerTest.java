0740298dc9e0b737d62b9f774506afbadb71d70f
jidelson <joeidelson@gmail.com>
2022-08-16 08:40:52 -0500

    tested index controller

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+
+        String viewName = controller.getIndexPage(model);
+        assertEquals("index",  viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
